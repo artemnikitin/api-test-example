a5a2472cd51c1d2c576a7a82976148c9926cc86e
Artem Nikitin <artemnikitin@artemnikitin.com>
2017-05-10 00:18:00 +0200

    Simplify config

--- src/main/java/com/artemnikitin/api/tests/config/Config.java
+++ src/main/java/com/artemnikitin/api/tests/config/Config.java
@@ -7,7 +7,7 @@ import java.io.File;
 public class Config {
 
     private static final com.typesafe.config.Config cfg = ConfigFactory.parseFile(
-            new File("api-tests.conf"));
+            new File("api-tests.conf")).resolve();
 
     public static String getHost() {
         String env = System.getenv("VULNERS_HOST");
@@ -23,14 +23,12 @@ public class Config {
     public static String getUrl() {
         String env = System.getenv("VULNERS_URL");
         String property = System.getProperty("vulners.url", "");
-        if (env != null) {
+        if (env != null)
             return env;
-        } else if (!property.equals("")) {
+        else if (!property.equals(""))
             return property;
-        } else {
-            com.typesafe.config.Config config = cfg.resolve();
-            return config.getString("api-tests.vulners.url");
-        }
+        else
+            return cfg.getString("api-tests.vulners.url");
     }
 
 }
